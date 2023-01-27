8cc6c19a91bed6496efbb5941c35ece0ca912ce0
Malithi Karunarathne <mkarunarathne997@gmail.com>
2023-01-27 22:59:08 +0530

    new order

--- src/pages/ward/Order/index.jsx
+++ src/pages/ward/Order/index.jsx
@@ -3,58 +3,24 @@ import {
   Button,
   Grid,
   IconButton,
-  Modal,
   TextField,
   Typography,
 } from "@mui/material";
-import { Box, Stack } from "@mui/system";
-import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
+import { Box } from "@mui/system";
+
 import { useEffect, useState } from "react";
 import TitleBar from "../../../components/TitleBar";
 import order from "../../../images/order.png";
-import Chart from "react-apexcharts";
-import { useForm } from "react-hook-form";
-import { getOrders, newOrder } from "../../../App/orderService";
-import EnhancedTable from "../../../components/Tables/EnhancedTable";
 import { getDrugById } from "../../../App/drugsService";
 import DeleteIcon from "@mui/icons-material/Delete";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import ClearAllIcon from "@mui/icons-material/ClearAll";
+import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { newOrder } from "../../../App/orderService";
 
 const Order = () => {
-  const [indexes, setIndexes] = useState([]);
-  const [counter, setCounter] = useState(0);
   const [drugs, setDrugs] = useState([]);
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data.orderItems);
-  };
-
-  const addItem = () => {
-    setIndexes((prevIndexes) => [...prevIndexes, counter]);
-    setCounter((prevCounter) => prevCounter + 1);
-  };
-
-  const removeItem = (index) => () => {
-    setIndexes((prevIndexes) => [
-      ...prevIndexes.filter((item) => item !== index),
-    ]);
-    setCounter((prevCounter) => prevCounter - 1);
-  };
-
-  const clearItems = () => {
-    setIndexes([]);
-  };
+  const [quantityOrdered, setQuantityOrdered] = useState("");
+  const [value, setValue] = useState("");
 
   useEffect(() => {
     getDrugById((response) => {
@@ -62,6 +28,31 @@ const Order = () => {
       setDrugs(response.drug);
     });
   }, []);
+
+  const { handleSubmit } = useForm();
+
+  const [orderItems, setOrderItems] = useState([]);
+
+  var today = new Date(),
+    month = today.getMonth() + 1,
+    month1 = month < 10 ? "0" + month : month,
+    date = today.getFullYear() + "-" + month1 + "-" + today.getDate();
+
+  const wardUser = useSelector((state) => state.loginHPMS._id);
+
+  const requestBody = {
+    wardUser: wardUser,
+    date: date,
+    orderItems: orderItems,
+  };
+  const onSubmit = () => {
+    console.log(requestBody);
+    newOrder(requestBody, (response) => {
+      console.log(response);
+    });
+    setOrderItems([]);
+  };
+
   return (
     <Box>
       <TitleBar image={order} title="Order" description="Manages Orders" />
@@ -88,128 +79,72 @@ const Order = () => {
                   >
                     Order
                   </Typography>
-
                   <Grid item lg={12}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                      {indexes.map((index) => {
-                        const fieldName = `orderItems[${index}]`;
+                    <Typography>Drug</Typography>
+                    <Autocomplete
+                      disablePortal
+                      // {...register("drug", {
+                      //   required: {
+                      //     value: true,
+                      //     message: "Drug is required",
+                      //   },
+                      // })}
+                      onChange={(e, value) => {
+                        setValue(value);
+                        console.log(value);
+                      }}
+                      id="combo-box-demo"
+                      getOptionLabel={(option) => option.drugId}
+                      options={drugs}
+                      sx={{
+                        mt: "0.5rem",
+                        width: "100%",
+                        // ...(errors.drug && {
+                        //   border: "1px solid red",
+                        // }),
+                      }}
+                      renderInput={(params) => {
                         return (
-                          <fieldset
-                            style={{ border: "none" }}
-                            name={fieldName}
-                            key={fieldName}
-                          >
-                            <Grid container spacing={2}>
-                              <Grid item lg={6.3} xs={12}>
-                                <Typography variant="h7" fontWeight={"normal"}>
-                                  Drug {index}:
-                                </Typography>
-                                {/* <input
-                            type="text"
-                            name={`${fieldName}.firstName`}
-                            {...register(`${fieldName}.firstName`)}
-                          /> */}
-                                <Autocomplete
-                                  disablePortal
-                                  {...register(`${fieldName}.drug`, {
-                                    required: {
-                                      value: true,
-                                      message: "Drug is required",
-                                    },
-                                  })}
-                                  onChange={(e, value) => {
-                                    setValue("drug", value);
-                                    console.log(value);
-                                  }}
-                                  id="combo-box-demo"
-                                  getOptionLabel={(option) => option.drugId}
-                                  options={drugs}
-                                  sx={{
-                                    mt: "0.5rem",
-                                    width: "100%",
-                                    ...(errors.drug && {
-                                      border: "1px solid red",
-                                    }),
-                                  }}
-                                  renderInput={(params) => {
-                                    return (
-                                      <TextField
-                                        name={`${fieldName}.drug`}
-                                        sx={{ color: "red" }}
-                                        {...params}
-                                        size="small"
-                                        InputProps={{
-                                          ...params.InputProps,
-                                          type: "search",
-                                        }}
-                                      />
-                                    );
-                                  }}
-                                />
-                              </Grid>
-                              <Grid item lg={4.3} xs={12}>
-                                <Typography variant="h7" fontWeight={"normal"}>
-                                  Quantity {index}:
-                                </Typography>
-                                <TextField
-                                  size="small"
-                                  sx={{ width: "100%", mt: "0.5rem" }}
-                                  type="text"
-                                  name={`${fieldName}.quantityOrdered`}
-                                  {...register(`${fieldName}.quantityOrdered`)}
-                                />
-                              </Grid>
-                              <Grid item lg={1.4}>
-                                <IconButton
-                                  aria-label="delete"
-                                  onClick={removeItem(index)}
-                                  sx={{ mt: "1.8rem" }}
-                                >
-                                  <DeleteIcon />
-                                </IconButton>
-                              </Grid>
-                            </Grid>
-                          </fieldset>
+                          <TextField
+                            sx={{ color: "red" }}
+                            {...params}
+                            size="small"
+                            InputProps={{
+                              ...params.InputProps,
+                              type: "search",
+                            }}
+                          />
                         );
-                      })}
-
-                      <Grid item lg={12}>
-                        <Grid container spacing={2} p={2}>
-                          <Grid item lg={4} xs={12}>
-                            <Button
-                              variant="contained"
-                              onClick={addItem}
-                              startIcon={<AddCircleIcon />}
-                              sx={{ width: "100%" }}
-                            >
-                              Add New
-                            </Button>
-                          </Grid>
-                          <Grid item lg={4} xs={12}>
-                            <Button
-                              variant="contained"
-                              onClick={clearItems}
-                              startIcon={<ClearAllIcon />}
-                              sx={{ width: "100%" }}
-                            >
-                              Clear All
-                            </Button>
-                          </Grid>
-                          <Grid item lg={4} xs={12}>
-                            <Button
-                              type="submit"
-                              variant="contained"
-                              color="primary"
-                              startIcon={<AddShoppingCartIcon />}
-                              sx={{ width: "100%" }}
-                            >
-                              Order
-                            </Button>
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                    </form>
+                      }}
+                    />
                   </Grid>
+                  <Grid item lg={12}>
+                    <Typography>Quantity</Typography>
+                    <TextField
+                      size="small"
+                      fullWidth
+                      id="quantityOrdered"
+                      value={quantityOrdered}
+                      onChange={(e) => setQuantityOrdered(e.target.value)}
+                    />
+                  </Grid>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      setQuantityOrdered("");
+                      setValue("");
+                      setOrderItems([
+                        ...orderItems,
+                        {
+                          drug: value,
+                          quantityOrdered: quantityOrdered,
+                        },
+                      ]);
+                      console.log(orderItems);
+                    }}
+                  >
+                    Order
+                  </Button>
                 </Grid>
               </Box>
             </Grid>
@@ -220,6 +155,45 @@ const Order = () => {
             <Typography variant="h6" fontWeight={"bold"} color="#495579" pb={1}>
               Order
             </Typography>
+            <Grid container>
+              {orderItems.map((item) => {
+                return (
+                  <Grid item lg={12} key={item.id}>
+                    <Grid container>
+                      <Grid item lg={4}>
+                        <Typography>Drug</Typography>
+                        <Typography>{item.drug.drugId}</Typography>
+                      </Grid>
+                      <Grid item lg={4}>
+                        <Typography>Quantity</Typography>
+                        <Typography>{item.quantityOrdered}</Typography>
+                      </Grid>
+                      <Grid item lg={4}>
+                        <IconButton
+                          onClick={() => {
+                            setOrderItems(
+                              orderItems.filter(
+                                (i) => i.drug.drugId !== item.drug.drugId
+                              )
+                            );
+                            console.log(orderItems);
+                          }}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                );
+              })}
+              {orderItems.length > 0 ? ( // if orderItems is not empty
+                <Button variant="contained" onClick={handleSubmit(onSubmit)}>
+                  Submit
+                </Button>
+              ) : (
+                <Typography>Order is empty</Typography>
+              )}
+            </Grid>
           </Box>
         </Grid>
       </Grid>
